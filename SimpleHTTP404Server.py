bdc5903a70b45f30b11479d166506373bd806d01
Patrick Cloke <clokep@gmail.com>
2015-02-24 00:10:09 -0500

    Fix 404 pages for fake server.

--- SimpleHTTP404Server.py
+++ SimpleHTTP404Server.py
@@ -5,12 +5,9 @@ import SimpleHTTPServer
 class GitHubHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     """
     Overrides the default request handler to handle GitHub custom 404 pages.
-    (Pretty much a 404.html page in your root.)
+    (I.e. a 404.html page located in the root.)
 
     See https://help.github.com/articles/custom-404-pages
-
-    This currently only works for erroneous pages in the root directory, but
-    that's enough to test what the 404 page looks like.
 
     """
     def do_GET(self):
@@ -18,10 +15,39 @@ class GitHubHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         # If the path doesn't exist, fake it to be the 404 page.
         if not os.path.exists(path):
-            self.path = '404.html'
+            path = '404.html'
+            # This is based on ``send_head`` of ``SimpleHTTPRequestHandler``.
+            ctype = self.guess_type(path)
+            try:
+                # Always read in binary mode. Opening files in text mode may cause
+                # newline translations, making the actual size of the content
+                # transmitted *less* than the content-length!
+                f = open(path, 'rb')
+
+                try:
+                    # Ensure we return a 404.
+                    self.send_response(404)
+                    self.send_header("Content-type", ctype)
+                    fs = os.fstat(f.fileno())
+                    self.send_header("Content-Length", str(fs[6]))
+                    self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+                    self.end_headers()
+
+                    # Send the actual content.
+                    self.copyfile(f, self.wfile)
+
+                    # Stop processing.
+                    return
+                except:
+                    f.close()
+                    raise
+            except IOError:
+                # 404.html doesn't exist, default back to the superclass.
+                pass
 
         # Call the superclass methods to actually serve the page.
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 
-SimpleHTTPServer.test(GitHubHandler)
+if __name__ == '__main__':
+    SimpleHTTPServer.test(GitHubHandler)
