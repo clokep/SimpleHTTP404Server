3a5df09e6bfa1d64d8d2543ecdcb2dd29d84cbe4
Patrick Cloke <clokep@gmail.com>
2015-05-13 23:11:42 -0400

    Go back to the simple server, the complicated version has no benefits.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,10 @@
+v0.2.0: May 13, 2015
+====================
+
+* Switch back to a simple way of getting the 404.html page. The complicated way
+  seems to imply it fixed server 404 pages from non-root directories...but it
+  doesn't, ensure your paths are absolute.
+
 v0.1.2: May 13, 2015
 ====================
 
--- SimpleHTTP404Server.py
+++ SimpleHTTP404Server.py
@@ -15,35 +15,7 @@ class SimpleHTTP404RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
         # If the path doesn't exist, fake it to be the 404 page.
         if not os.path.exists(path):
-            path = '404.html'
-            # This is based on ``send_head`` of ``SimpleHTTPRequestHandler``.
-            ctype = self.guess_type(path)
-            try:
-                # Always read in binary mode. Opening files in text mode may cause
-                # newline translations, making the actual size of the content
-                # transmitted *less* than the content-length!
-                f = open(path, 'rb')
-
-                try:
-                    # Ensure we return a 404.
-                    self.send_response(404)
-                    self.send_header("Content-type", ctype)
-                    fs = os.fstat(f.fileno())
-                    self.send_header("Content-Length", str(fs[6]))
-                    self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-                    self.end_headers()
-
-                    # Send the actual content.
-                    self.copyfile(f, self.wfile)
-
-                    # Stop processing.
-                    return
-                except:
-                    f.close()
-                    raise
-            except IOError:
-                # 404.html doesn't exist, default back to the superclass.
-                pass
+            self.path = '404.html'
 
         # Call the superclass methods to actually serve the page.
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
